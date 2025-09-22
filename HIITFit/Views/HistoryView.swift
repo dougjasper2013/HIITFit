92b15b46cae64a0c587b2c80c85501971d3f80f7
Douglas Jasper <doug.jasper@trios.com>
2025-09-22 09:10:11 -0400

    Added HistoryView Dismiss Button

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -35,21 +35,28 @@ import SwiftUI
 struct HistoryView: View {
     let history = HistoryStore()
     var body: some View {
-        VStack {
-          Text("History")
+        ZStack(alignment: .topTrailing) {
+            Button(action: {}) {
+              Image(systemName: "xmark.circle")
+            }
             .font(.title)
-            .padding()
-            Form {
-              ForEach(history.exerciseDays) { day in
-                Section(
-                  header:
-                    Text(day.date.formatted(as: "MMM d"))
-                    .font(.headline)) {
-                  ForEach(day.exercises, id: \.self) { exercise in
-                    Text(exercise)
-                  }
+            .padding(.trailing)
+            VStack {
+                Text("History")
+                    .font(.title)
+                    .padding()
+                Form {
+                    ForEach(history.exerciseDays) { day in
+                        Section(
+                            header:
+                                Text(day.date.formatted(as: "MMM d"))
+                                .font(.headline)) {
+                                    ForEach(day.exercises, id: \.self) { exercise in
+                                        Text(exercise)
+                                    }
+                                }
+                    }
                 }
-              }
             }
         }
     }
