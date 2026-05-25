3ba21f1ba40499181a9c92e790a25994ac192fea
Douglas Jasper <doug.jasper@trios.com>
2026-05-25 08:14:01 -0400

    Added erercises Array

--- HIITFit/Exercise.swift
+++ HIITFit/Exercise.swift
@@ -11,3 +11,12 @@ struct Exercise {
         case sunSalute = "Sun Salute"
     }
 }
+
+extension Exercise {
+    static let exercises = [
+        Exercise(exerciseName: ExerciseEnum.squat.rawValue, videoName: "squat"),
+        Exercise(exerciseName: ExerciseEnum.stepUp.rawValue, videoName: "step-up"),
+        Exercise(exerciseName: ExerciseEnum.burpee.rawValue, videoName: "burpee"),
+        Exercise(exerciseName: ExerciseEnum.sunSalute.rawValue, videoName: "sune-salute")
+        ]
+}
