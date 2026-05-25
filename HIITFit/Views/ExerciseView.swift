6225e0605a261025c2867e2738475de10f10e400
Douglas Jasper <doug.jasper@trios.com>
2026-05-25 09:35:28 -0400

    Updated VideoPlayerView and Exercise View

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -1,6 +1,5 @@
 
 import SwiftUI
-import AVKit
 
 struct ExerciseView: View {
     let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
@@ -19,12 +18,9 @@ struct ExerciseView: View {
             VStack {
                 HeaderView(exerciseName: exercise.exerciseName)
                     .padding(.bottom)
-                if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
-                    VideoPlayer(player: AVPlayer(url: url))
-                        .frame(height: geometry.size.height * 0.45)
-                } else {
-                    Text("Coulldn't find \(exercise.videoName).mp4").foregroundColor(.red)
-                }
+                VideoPlayerView(videoName: exercise.videoName)
+                    .frame(height: geometry.size.height * 0.35)
+                    .padding(20)
                 Text(Date().addingTimeInterval(interval), style: .timer)
                     .font(.system(size: geometry.size.height * 0.07))
                 Button("Start/Done") {
