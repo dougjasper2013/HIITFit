6ca4ad86e7d7291df0bf0a6437ec422e7af1ddd8
Douglas Jasper <doug.jasper@trios.com>
2026-05-26 09:29:14 -0400

    Started SuccessView

---
+++ HIITFit/Views/SuccessView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SuccessView()
+}
